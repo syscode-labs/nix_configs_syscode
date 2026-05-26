4f11bc0dd981267f50befe545ecae1b707615e6b
Giovanni Ferri <giovanni@syscode.uk>
2026-05-26 15:33:56 +0100

    titan: revert accidental quiet/loglevel params; fix console redirect to tty12
    quiet + loglevel=3 suppressed initrd output, breaking YubiKey LUKS challenge
    detection. Reverts kernelParams to mem_sleep_default=s2idle only.
    
    console-to-tty12: setconsole was removed in kbd 2.x; replace with Python
    TIOCCONS ioctl one-liner which redirects the kernel console device to /dev/tty12.

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -19,7 +19,9 @@
     wantedBy = [ "greetd.service" ];
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = "${pkgs.kbd}/bin/setconsole /dev/tty12";
+      ExecStart = pkgs.writeShellScript "redirect-console" ''
+        ${pkgs.python3}/bin/python3 -c 'import fcntl,os,termios; fd=os.open("/dev/tty12",os.O_RDWR|os.O_NOCTTY); fcntl.ioctl(fd,termios.TIOCCONS); os.close(fd)'
+      '';
     };
   };
 
